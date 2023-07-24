1cc0ba27b0a7a9aad1dec63592dff2ed13d11735
Abukmayen <abukmayen123@gmail.com>
2023-07-24 02:06:29 -0700

    2-variable_load.py

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-if _name_ == "_main_":
-    """Print the value of variable a from variable_load_2."""
-    from variable_load_2 import a
 
+if __name__ == "__main__":
+    # Import the variable 'a' from variable_load_2.py
+    from variable_load_2. import a
+
+    #print the value of 'a'
     print(a)
