a712f276fb4b4086eb8db11ffe19771505684c25
Abukmayen <abukmayen123@gmail.com>
2023-07-24 01:57:42 -0700

    5-raise_exception_msg.py

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pytnon3
+#!/usr/bin/python3
 
 
 def raise_exception_msg(message=""):
@@ -6,6 +6,6 @@ def raise_exception_msg(message=""):
 
 if __name__ == "__main__":
     try:
-        def raise_exception_msg("C is fun")
-    except- NameError as ne:
+        raise_exception_msg("C is fun")
+    except NameError as ne:
         print(ne)
