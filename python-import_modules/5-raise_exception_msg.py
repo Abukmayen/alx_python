9746e067091be642ab22a170a8fc106082e6c19a
Abukmayen <abukmayen123@gmail.com>
2023-07-24 01:49:09 -0700

    Write a function that raises a name exception with a message.

---
+++ python-import_modules/5-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pytnon3
+
+
+def raise_exception_msg(message=""):
+    raise NameError(message)
+
+if __name__ == "__main__":
+    try:
+        def raise_exception_msg("C is fun")
+    except- NameError as ne:
+        print(ne)
