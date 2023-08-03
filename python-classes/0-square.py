198d072aa022644bd5ed5ba36958358ca8e81999
Abukmayen <abukmayen123@gmail.com>
2023-08-03 00:41:18 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
+"""
+Module - 0-square.py
+Defines a class Square with a private instance attribute size
+"""
+
+
 class Square:
+    """
+    A class Square with private instance attribute size
+    """
     def __init__(self, size):
+        """
+        Initialize a new Square with the given size
+
+        Args:
+        size (int): The size of the new square object.
+
+        Returns:
+        None
+        """
         self.__size = size
-Square = __import__('0-square').Square
-
-# Test case 1
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
-
-# Test case 2
-mysquare = Square(89)
-print(type(mysquare))
-print(mysquare.__dict__)
-
-# Test case 3
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
-try:
-    print(mysquare.size)
-except Exception as e:
-    print(e)
-
-# Test case 4
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
-try:
-    print(mysquare._size)
-except Exception as e:
-    print(e)
