80725c70b6c25a33d379faf2b363554143382343
Abukmayen <abukmayen123@gmail.com>
2023-08-02 23:49:18 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,33 @@
 #!/usr/bin/python3
-"""This module contains an empty class Square"""
-
-
 class Square:
-    """An empty class Square"""
-    pass
+    def __init__(self, size):
+        self.__size = size
+Square = __import__('0-square').Square
+
+# Test case 1
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.__dict__)
+
+# Test case 2
+mysquare = Square(89)
+print(type(mysquare))
+print(mysquare.__dict__)
+
+# Test case 3
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.__dict__)
+try:
+    print(mysquare.size)
+except Exception as e:
+    print(e)
+
+# Test case 4
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.__dict__)
+try:
+    print(mysquare._size)
+except Exception as e:
+    print(e)
