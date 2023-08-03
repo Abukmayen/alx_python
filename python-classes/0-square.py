ee0370d57747eec29735b9c17cd657a359e1bb59
Abukmayen <abukmayen123@gmail.com>
2023-08-02 18:14:35 -0700

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,40 @@
 #!/usr/bin/python3
+"""
+New class Square
+"""
+
+
 class Square:
     def __init__(self, size):
         self.__size = size
+
+
+Square = __import__('0-square').Square
+
+# Test case 1
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.__dict__)
+
+# Test case 2
+mysquare = Square(89)
+print(type(mysquare))
+print(mysquare.__dict__)
+
+# Test case 3
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.__dict__)
+try:
+    print(mysquare.size)
+except Exception as e:
+    print(e)
+
+# Test case 4
+mysquare = Square(3)
+print(type(mysquare))
+print(mysquare.__dict__)
+try:
+    print(mysquare._size)
+except Exception as e:
+    print(e)
